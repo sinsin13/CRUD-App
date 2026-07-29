5c219e7fab0ec51ebdaeeab000c9b35a9a920b50
sinsin13 <adsnh13@gmail.com>
2026-07-30 01:18:13 +0530

    del

--- server/routes/book.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleBookStoreController } = require('../controller/book.controller');
-
-// http://localhost:8000/book/addbook
-router.post("/addbook", handleBookStoreController);
-
-module.exports = router;
